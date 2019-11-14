7635ebfe82b5dd338e9f382471719504e6f61f63
Zhang Shuyang <zhangshuyang@outlook.com>
2019-11-14 17:28:33 +0800

    added a timedelta for sending exit signal

--- test.py
+++ test.py
@@ -69,4 +69,5 @@ else:
             random_walk(hwnd)
             random_jump(hwnd)
         else:
-            terminate_app("notepad.exe")
+            terminate_app("notepad.exe")\
+            time.sleep(5)
